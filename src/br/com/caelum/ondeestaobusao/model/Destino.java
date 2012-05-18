6d54be33d562e5b464e43a28bbbdecfecc56cf0c
André Silva <andre.silva@caelum.com.br>
2012-05-18 17:07:58 -0300

    Nova classe no modelo que representa um destino

---
+++ src/br/com/caelum/ondeestaobusao/model/Destino.java
@@ -0,0 +1,29 @@
+package br.com.caelum.ondeestaobusao.model;
+
+import java.util.ArrayList;
+
+public class Destino {
+	private Coordenada coordenada;
+	private String endereco;
+	private ArrayList<Ponto> pontos;
+	
+	public Coordenada getCoordenada() {
+		return coordenada;
+	}
+	public void setCoordenada(Coordenada coordenada) {
+		this.coordenada = coordenada;
+	}
+	public ArrayList<Ponto> getPontos() {
+		return pontos;
+	}
+	public void setPontos(ArrayList<Ponto> pontos) {
+		this.pontos = pontos;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+}
