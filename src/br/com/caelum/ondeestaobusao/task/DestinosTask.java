ae507bb0b60badc7338f80e38dfb3a668260e40a
André Silva <andre.silva@caelum.com.br>
2012-05-18 17:01:52 -0300

    Mudando nome de CoordenadaDoEndereco para DestinoTask e colocando códigos de busca corretos

---
+++ src/br/com/caelum/ondeestaobusao/task/DestinosTask.java
@@ -0,0 +1,47 @@
+package br.com.caelum.ondeestaobusao.task;
+
+import java.lang.reflect.Type;
+import java.net.URLEncoder;
+
+import br.com.caelum.ondeestaobusao.delegate.AsyncResultDelegate;
+import br.com.caelum.ondeestaobusao.delegate.GetJsonDelegate;
+import br.com.caelum.ondeestaobusao.model.Destino;
+
+import com.google.gson.reflect.TypeToken;
+
+public class DestinosTask implements GetJsonDelegate<String, Destino> {
+//	private final String server_url = "http://ondeestaoalbi.herokuapp.com/destinoParaEndereco/%s";
+	private final String server_url = "http://192.168.84.100:3000/destinoParaEndereco/%s.json";
+	private final AsyncResultDelegate<Destino> delegate;
+	
+	public DestinosTask(AsyncResultDelegate<Destino> delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public void doOnPostExecute(Destino result) {
+		delegate.dealWithResult(result);
+	}
+
+	@Override
+	public String getFormatedURL(String... params) {
+		String uri = URLEncoder.encode(params[0]);
+		return String.format(server_url, uri);
+	}
+
+	@Override
+	public Type getElementType() {
+		return new TypeToken<Destino>() {}.getType();
+	}
+
+	@Override
+	public void doOnError(Exception e) {
+		delegate.dealWithError();
+	}
+
+	@Override
+	public Destino onErrorReturn() {
+		return null;
+	}
+
+}
